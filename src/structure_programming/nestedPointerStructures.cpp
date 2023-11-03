e139cdfc85104d2c54628ac5910004cd5807c61d
Mansoor Lodhi <malodhi97@gmail.com>
2023-11-03 01:26:36 +0100

    structures completed

--- src/structure_programming/nestedPointerStructures.cpp
+++ src/structure_programming/nestedPointerStructures.cpp
@@ -14,20 +14,33 @@ struct family_members{
 struct employee{
     string name;
     int taxNumber;
-    family_members relationships;
-    company *Employer;
+    company Employer;
+    family_members *relationships;
 };
 
 
 int main(){
-    // note: we cannot directly declare or initiate a struct pointer
-    // the only way is to create an object/variable of type struct
+    // note: we cannot directly declare or initiate a nested struct pointer
+    // the only way is to create an object/variable of type (main) struct
     // and in parallel declare and instantiate a pointer of type struct
+
+    //accessing nested struct through pointers:
     employee Mansoor {"Mansoor", 05214, "KPMG", "London"};
+    
     employee *MansoorPt = &Mansoor;
+    
     MansoorPt->taxNumber = 0000;
+    
     cout << MansoorPt->taxNumber << endl;
     cout << MansoorPt->Employer.name << endl;
+
+    //accessing nested pointer struct through pointers:
+    family_members familyMembers {"myFather", "myMother"};
     
+    MansoorPt->relationships = &familyMembers; 
+
+    cout << MansoorPt->name << endl;
+    cout << MansoorPt->relationships->father << endl;
+       
 
 }
